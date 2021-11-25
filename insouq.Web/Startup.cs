d6c60371104060f5e627bdcd61d1700e19d13150
ghaith17 <ghaithsuleiman04@gmail.com>
2021-11-25 12:11:24 +0200

    25.11.2021

--- insouq.Web/Startup.cs
+++ insouq.Web/Startup.cs
@@ -89,11 +89,21 @@ namespace insouq.Web
             services.AddControllersWithViews();
 
             services.AddRazorPages().AddRazorRuntimeCompilation();
+            services.AddHttpsRedirection(options =>
+            {
+                options.HttpsPort = 443;
+            });                           // <=== Add this (AddHttpsRedirection) =====
+
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
+           
+
+            app.UseHttpsRedirection();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
